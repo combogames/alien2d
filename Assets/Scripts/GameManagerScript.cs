ed0240629eb1e910f85e456818eb1349ade5a3fa
Daniel Novi <daniel9c@gmail.com>
2017-01-27 08:31:49 -0200

    Adicionando botão para iniciar o game
    Criando 2 cenas, adicionando um botão na primeira com uma ação que
    chama a segunda cena.

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour {
+
+	/*
+	 * Carrega uma cena 
+	 */
+	public void LoadGameScene(){
+		SceneManager.LoadScene ("scene2");
+	}
+}
