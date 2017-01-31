6acfba4ab83d164e7f749c8370686e7f870a3c50
Daniel Novi <daniel9c@gmail.com>
2017-01-30 22:00:40 -0200

    Adicionando 2 naves
    Duas naves que ficam voando no menu principal.

---
+++ Assets/Scripts/AlienShipGreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienShipGreen : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed = 5f;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		TranslateAlien ();
+	}
+
+	void TranslateAlien() {
+		transform.Translate (Vector2.right * speed * Time.deltaTime);
+	}
+}
