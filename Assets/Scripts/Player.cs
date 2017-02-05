ab456600cca57f2afd1a4d802d1a4cd2e7e55c30
Daniel Novi <daniel9c@gmail.com>
2017-02-05 10:37:31 -0200

    Adicionando movimento para o Player
    E também detecção de colisão com o chão.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,70 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
+	private float speed = 50f;
+	private float maxSpeed = 3;
+	private float JumpPower = 250f;
+	public bool grounded = false; //<<Verificar PlayerCheck.cs para grounded
+
+	private Rigidbody2D rb2d;
+	private Animator anim;
+
+	//Inicializando componentes e variaveis
+	//@override
 	void Start () {
-		
+		rb2d = GameObject.FindObjectOfType<Rigidbody2D> ();
+		anim = GameObject.FindObjectOfType<Animator> ();
 	}
 	
-	// Update is called once per frame
+	//Update is called once per frame
+	//@override
 	void Update () {
-		
+
+		//'anim' seta a animacao apropriada do player para determinada acao.
+		//Estas animacoes e parametros estao configuradas dentro do Animator do player
+		anim.SetBool ("groundedParam", grounded); 
+
+		//Mathf.Abs retorna um valor absoluto e positivo, ficando um numero menor e mais maleavel.
+		anim.SetFloat ("speedParam", Mathf.Abs (Input.GetAxis ("Horizontal")));
+
+	}
+
+	//FixedUpdate nao sofre interferencia do device ao atualizar
+	//sendo sempre fixo e melhor para interagir com controles
+	//@override
+	void FixedUpdate(){
+		MovePlayer ();
+	}
+
+	//Movimenta o personagem com as teclas/inputs
+	void MovePlayer(){
+
+		//movendo na horizontal com as teclas
+		float h = Input.GetAxis ("Horizontal");
+		rb2d.AddForce ((Vector2.right * speed) * h);
+
+		//Limitando a velocidade do player, caso contrario ele fica muito rápido
+		if (rb2d.velocity.x > maxSpeed) {
+			rb2d.velocity = new Vector2 (maxSpeed, rb2d.velocity.y);
+		}		
+		if (rb2d.velocity.x < -maxSpeed) {
+			rb2d.velocity = new Vector2 (-maxSpeed, rb2d.velocity.y);
+		}		
+
+		//Pula ao apertar espaço no teclado, se o personagem estiver no chao
+		if (Input.GetButtonDown ("Jump") && grounded) {
+			rb2d.AddForce (Vector2.up * JumpPower);
+		}
+
+		invertPlayerSpriteByDirection (h);
+	}
+
+	//Inverte o sprite de lado para ao andar para a esquerda ou direita
+	private void invertPlayerSpriteByDirection(float horizAxis){
+		if (horizAxis < -0.1f) {
+			transform.localScale = new Vector3 (-1, 1, 1);
+		}else if (horizAxis > 0.1f) {
+			transform.localScale = new Vector3 ( 1, 1, 1);
+		}	
 	}
 }
---
+++ Assets/Scripts/PlayerGroundCheck.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGroundCheck : MonoBehaviour {
+
+	//Referenciando o Script Player.cs
+	private Player player;
+
+	//@override
+	void Start(){
+		
+		//Instanciando o Script Player.cs
+		//Como este script está contido no objeto filho do Player
+		//foi usado o comando GetComponentInParent 
+ 		player = gameObject.GetComponentInParent<Player> (); 
+
+	}
+
+	//Nota, para usar este tipo de colisao 'trigger'
+	//o collider2d tem que estar setado como 'is trigger'
+	//@override
+	void OnTriggerEnter2D(Collider2D col){
+		player.grounded = true;
+	}
+
+	//@override
+	void OnTriggerStay2D(Collider2D col){
+		player.grounded = true;
+	}
+
+	//@override
+	void OnTriggerExit2D(Collider2D col){
+		player.grounded = false;
+	}
+
+}
